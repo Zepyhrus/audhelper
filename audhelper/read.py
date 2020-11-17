990267eb56b8697ba6136ab7a16cbfe16cb3b8cf
Zepyhrus <sherkfung@gmail.com>
2020-11-17 15:16:21 +0800

    add new nread and augmentation

--- audhelper/read.py
+++ audhelper/read.py
@@ -4,6 +4,28 @@ import wave
 import soundfile as sf
 from samplerate import resample
 import numpy as np
+from scipy.io import wavfile
+
+# augmentation
+# normalized_read: around 0.5046 s/1000 steps;
+# nread: 0.1522
+# AudDatasets: around 1159 s for reading 1592226 datasets, 12439 batches, 0.7279 s/1000 steps;
+# train: around 200 min/12000 s for 51460 batches, 6586880 steps, 1.8218 s/1000 steps;
+from audiomentations import (
+  Compose,
+  Gain,                   #  0.1701   0.1086
+  AddGaussianNoise,       #  1.0177   0.7528
+  TimeStretch,            # 10.8556   8.4630
+  PitchShift,             # 29.8609  23.1933
+  Shift,                  #  0.1838   0.1261
+  AddImpulseResponse,     #  3.3720   1.8873
+  FrequencyMask,          #  2.4121   1.8783
+  TimeMask,               #  0.1652   0.1133
+  AddGaussianSNR,         #  1.1160   0.8297
+  ClippingDistortion,     #  0.5428   0.4300
+  AddBackgroundNoise,     #  1.7820   1.7376
+  AddShortNoises          #  1.6541   1.6303
+)
 
 
 #@title Helper functions and classes
@@ -47,6 +69,94 @@ def normalized_read(filename, target_samples, target_sample_rate, shuffle):
   normalized_samples = np.clip(normalized_samples, -1, 1)
 
   return normalized_samples
+
+class MyGain(Gain):
+  def apply(self, samples, sample_rate):
+    return np.clip(samples * self.parameters["amplitude_ratio"], -1, 1)
+
+
+def compose(sounds_path):
+  _p = 0.2
+
+  transforms = [
+    MyGain(p=_p),
+    AddGaussianNoise(p=_p),
+    Shift(p=_p, min_fraction=-0.25, max_fraction=0.25),
+    FrequencyMask(p=_p),
+    TimeMask(p=_p, max_band_part=0.25),
+    AddGaussianSNR(p=_p),
+    ClippingDistortion(p=_p, max_percentile_threshold=20),
+    AddBackgroundNoise(sounds_path='data/background1', p=_p),
+    TimeStretch(p=_p/10),
+    PitchShift(p=_p/30),
+  ]
+  
+  return Compose(transforms, p=0.4, shuffle=True)
+
+
+def nread(data, samples, sample_rate, shuffle, aug=None):
+  # a faster implementation of normalize read with augmentation
+  if isinstance(data, str):
+    sr, raw = wavfile.read(data)
+    aud = raw / 32768
+  elif isinstance(data, np.ndarray):
+    aud = data.copy()
+    sr = sample_rate
+  else:
+    raise Exception('Wrong read format! Only supporting filename/numpy.ndarray.')
+  
+  # reduce to mono channel
+  if aud.ndim > 1: aud = aud.mean(axis=1)
+
+  # augmentation
+  if aug: aud = aug(aud, sr)
+
+  # resample
+  if sr != sample_rate: aud = resample(aud, sample_rate/sr)
+
+  _size = aud.shape[0]
+  # crop
+  if _size > samples:
+    idx = np.random.randint(_size - samples) if shuffle else ( - samples) // 2
+    aud = aud[idx:(idx+samples)]
+  
+  # pad
+  if _size < samples:
+    pad_left = np.random.randint(samples - _size) if shuffle else (samples - _size) // 2
+    pad_right = samples - _size - pad_left
+
+    aud = np.pad(aud, (pad_left, pad_right), 'constant')
+
+  # normalize
+  normalize_factor = max(1e-6, np.max(np.abs(aud)))
+  aud /= normalize_factor
+
+  return aud
+
+
+def aread(audio, method='sf', aug=None):
+  if method == 'sf':
+    aud, sr = sf.read(audio)
+  elif method == 'rosa':
+    aud, sr = rosa.load(audio)
+  elif method == 'dub':
+    song = AudioSegment.from_wav(audio)
+    aud = np.frombuffer(song.raw_data, dtype=np.short) / 32768
+    sr = song.frame_rate
+  elif method == 'sci':
+    sr, raw = wavfile.read(audio)
+    aud = raw / 32768
+  else:
+    raise Exception('Wrong audio read method!')
+
+  return aud, sr
+
+
+def awrite(audio, sample_rate, aud, method='sci'):
+  if method == 'sci':
+    wavfile.write(audio, sample_rate, (aud * 32768).astype(np.int16))
+  else:
+    raise Exception('Wrong audio read method!')
 
 
 class ReadLargeWav():
