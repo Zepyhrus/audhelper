b6565511a8853f899d3a93d879362ffb7694d71b
Zepyhrus <sherkfung@gmail.com>
2020-12-03 10:29:41 +0800

    remove stream_test, it is too slow

--- audhelper/stream.py
+++ audhelper/stream.py
@@ -1,10 +1,13 @@
+import threading, logging, queue, time
+
 import numpy as np
 from scipy.optimize import linear_sum_assignment as KM
 
 import soundfile as sf
 
 from .textgrid import TextGrid
-from .read import ReadLargeWav
+from .read import nread, StreamWav
+
 
 def textgrid_res(grid_file):
   text = open(grid_file).read()
@@ -41,38 +44,6 @@ def textgrid_res(grid_file):
   label_ed = np.array(label_ed)
 
   return labels, label_st, label_ed
-
-
-def stream_test(f, model, interval=100):
-  samples = model.samples
-  samplerate = model.sample_rate
-  overlap = samples - int(interval / 1000 * samplerate)
-  steps = int(samples / samplerate * 1000 / interval)
-  duration = int(samples / samplerate * 1000)
-
-  # frames = sf.blocks(f, blocksize=samples, overlap=overlap)
-  wav = ReadLargeWav(f)
-
-  res = [np.zeros((1, model.num_classes))]
-
-  cnt = 0
-  while True:
-    binary_data = wav.read(duration, interval)
-    if binary_data is not None:
-      frame = np.frombuffer(binary_data, dtype=np.short) / 32768.
-      preds = model.infer(frame)
-
-      res.append(preds)
-    else:
-      print(f'total {cnt * interval / 1000} s finished!')
-      break
-    
-    cnt += 1
-    if cnt % int(120 * 1000 / interval) == 0: print(f'{cnt * interval / 1000} s finished!')
-
-  wav.close()
-
-  return np.concatenate(res)
 
 
 def report(res, _c, _s, gamma):
@@ -170,6 +141,88 @@ def report_from_res(res_file, grid_file, interval=500, method='f1', word_index=1
         if target > best:
           best = target
         print(f'word {word_index} on {method} - recall: {recall:.2f}, precision: {precis:.2f}, f1: {f1:.2f} @ {_c}, {_g}, {len(t1)} labels, {len(t2) - correct} false alarms.')
+
+
+class WavQueue(queue.Queue):
+  """
+  The WavQueue holds the same use of Pipeline, but around 12% faster than Pipeline
+    WavQueue: 6.7s on room.wav, it is more than 70 times faster than stream_test
+    Pipeline: 7.8s on room.wav
+    stream_test: 9 min 18.1s on room.wav
+  Increasing batch size from 256 to 512, will slightly faciliate the speed, 6.7s to 6.5s
+  """
+  def __init__(self, model):
+    self.model = model
+    self.batch_size = self.model.batch_size
+    self.lock = threading.Lock()
+    super().__init__(maxsize=2) # 2 batch is enough
+  
+  def eat(self, file_name, res_file=None):
+    self.res_file = res_file
+    self.wav_file = file_name
+    self.wav = StreamWav(file_name=file_name, window=1500, stride=100)
+    self.res = np.zeros((self.wav.total_reads + 1, self.model.num_classes), dtype=np.float32)
+    self.res[0, 0] = 1
+    self.already_get = 0
+    self.batches = 0
+    self.total_batches = (self.wav.total_reads - 1) // self.batch_size + 1
+
+  def consume(self):
+    while True:
+      batches, batch_aud = self.get()
+
+      preds = self.model.batch_infer(batch_aud)
+
+      st = (batches - 1) * self.batch_size + 1
+      ed = st + len(batch_aud)
+
+      self.res[st:ed, :] = preds
+
+      if batches % 25 == 0: # usually 25 batch would be a fine update interval
+        logging.info('%.2f finished on %s batches...', batches / self.total_batches, self.total_batches)
+
+      if batches == self.total_batches: break
+    
+    if self.res_file:
+      logging.info('Res file saved to %s', self.res_file)
+      np.savetxt(self.res_file, self.res, delimiter=',')
+
+  def produce(self):
+    while True:
+      batch_idx = 0
+      batch_raw_data = []
+
+      # now we fetch raw data from wav file
+      with self.lock: # using lock to make sure all raw_data are read continously
+        while batch_idx < self.batch_size and self.already_get < self.wav.total_reads:
+          _, raw_data = self.wav.read()
+
+          if raw_data is None:
+            break
+          else:
+            batch_raw_data.append(raw_data)
+          
+          batch_idx += 1
+          self.already_get += 1
+      
+      # now we construct a batch of aud data
+      batch_aud = np.empty((batch_idx, self.model.samples), dtype=np.float32)
+
+      for i, _raw in enumerate(batch_raw_data):
+        batch_aud[i] = nread(
+          np.frombuffer(_raw, dtype=np.int16) / 32768.,
+          self.model.samples,
+          self.model.sample_rate,
+          shuffle=False,
+          aug=None
+        )
+      
+      self.batches += 1
+
+      self.put((self.batches, batch_aud))
+      if self.batches == self.total_batches: break
+
+
 
 if __name__ == "__main__":
   pass
